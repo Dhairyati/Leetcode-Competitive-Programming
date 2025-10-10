3d8b28844311cfcd32d45d4db3142e638248f1cd
Dhairyati <149410366+Dhairyati@users.noreply.github.com>
2025-10-10 22:36:03 +0530

    Create 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.java

---
+++ 3147_Taking_Maximum_Energy_From_the_Mystic_Dungeon.java
@@ -0,0 +1,19 @@
+/*
+3147. Taking Maximum Energy From the Mystic Dungeon
+*/
+
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int maxEnergy = -(1 << 30);  // -2^30
+        int n = energy.length;
+      
+        for (int endPosition = n-k; endPosition<n; endPosition++) {
+            int currentSum = 0;
+            for (int currentIndex=endPosition; currentIndex>=0; currentIndex-=k){
+                currentSum += energy[currentIndex];
+                maxEnergy = Math.max(maxEnergy, currentSum);
+            }
+        }
+        return maxEnergy;
+    }
+}
